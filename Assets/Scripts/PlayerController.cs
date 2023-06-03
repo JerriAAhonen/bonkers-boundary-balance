49f387f621d92d8983f2f69f3eeaf6e0c14f3709
Jerri Ahonen <jerri.ahonen@greenergrass.company>
2023-06-03 11:37:47 +0300

    Add horizontal movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float tapForce;
+    [SerializeField] private float horizontalMovementSpeed;
 
     private Rigidbody2D rb;
+    private bool pendingTap;
 
     private void Awake()
     {
@@ -17,7 +19,18 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(Vector2.up * tapForce);
+            pendingTap = true;
         }
+    }
+
+    private void FixedUpdate()
+    {
+        if (pendingTap)
+        {
+            rb.AddForce(Vector2.up * tapForce);
+            pendingTap = false;
+        }
+
+        rb.velocity = new Vector3(horizontalMovementSpeed, rb.velocity.y);
     }
 }
