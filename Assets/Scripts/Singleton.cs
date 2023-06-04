adec23453a1427e93e45616fe9ab1e401e84e24a
Jerri Ahonen <jerri.ahonen@greenergrass.company>
2023-06-04 09:51:05 +0300

    Add Singleton.cs

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : class
+{
+	public static T Instance { get; private set; }
+
+	protected virtual void Awake()
+	{
+		ProcessSingleton();
+	}
+		
+	private void  ProcessSingleton()
+	{
+		if (!Application.isPlaying) return;
+
+		if (Instance != null)
+		{
+			Destroy(gameObject); 
+			return;
+		}
+
+		Instance = this as T;
+		if (Instance == null)
+			throw new InvalidCastException($"Unable to cast {GetType()} to {typeof(T)}");
+	}
+}
